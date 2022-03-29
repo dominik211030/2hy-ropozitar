54881e3499d19bf2feedf1cf2b174b2867688d3f
dominik211030 <dompsik@gmail.com>
2022-03-29 09:42:41 +0200

    zase

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -6,7 +6,21 @@ def  is_whole_number(my_number):
     else:
         return False
 
+def is_even_number(my_number):
+    if my_number % 2 == 0:
+        return True
+    else:
+        return False
+
+my_numbers = [1,2,3,5,7,9]
+def  is_natural_number(my_numbers):
+    for my_number in my_numbers:
+        return my_number
+
+print(is_natural_number(my_number)):
+
 
 my_number = 5
 
 print(is_whole_number(my_number))
+print(is_even_number(my_number))
