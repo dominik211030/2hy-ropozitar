69cecd390a04a6ae0034105d8584837455c3cdf5
dominik211030 <dompsik@gmail.com>
2022-03-29 08:56:03 +0200

    ae

--- analyse_numbers.py
+++ analyse_numbers.py
@@ -1,12 +1,12 @@
 
 
 def  is_whole_number(my_number):
-    if my_number % 1 == 0:
+    if my_number % 1 == 0 and my_number > 0:
         return True
     else:
         return False
 
 
-my_number = 5.1
+my_number = 5
 
 print(is_whole_number(my_number))
